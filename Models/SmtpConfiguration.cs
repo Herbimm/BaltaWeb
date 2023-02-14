952940f965c201d8009f3b31e0ab10451c6d11ed
Herbert Carvalho <herbert.carvalho@cbyk.com.br>
2023-02-13 23:46:33 -0300

    feat Add host and port in bind Smtp Secrets

--- Models/SmtpConfiguration.cs
+++ Models/SmtpConfiguration.cs
@@ -2,6 +2,8 @@
 {
     public class SmtpConfiguration
     {
+        public string Host { get; set; }
+        public string Port{ get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
     }
